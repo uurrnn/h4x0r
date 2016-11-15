353e336111a78c9b23fcf159252866f875d9a4db
uurrnn <uurrnn@gmail.com>
2016-11-15 17:18:22 -0500

    changing over to es6

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import 'pixi';
+import 'arcade';
+import Phaser from 'phaser';
+
+import BootState from './states/boot';
+import SplashState from './states/preload';
+import GameState from './states/game';
+
+class Game extends Phaser.Game {
+
+  constructor () {
+    let width = document.documentElement.clientWidth > 768 ? 768 : document.documentElement.clientWidth
+    let height = document.documentElement.clientHeight > 1024 ? 1024 : document.documentElement.clientHeight
+
+    super(width, height, Phaser.AUTO, 'game', null)
+
+    this.state.add('Boot', BootState, false)
+    this.state.add('Splash', SplashState, false)
+    this.state.add('Game', GameState, false)
+
+    this.state.start('Boot')
+  }
+}
+
+window.game = new Game();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const webpack = require('webpack');
+const path = require('path');
+
+// Phaser webpack config
+const phaserModule = path.join(__dirname, '/node_modules/phaser/');
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.min.js');
+const pixi = path.join(phaserModule, 'build/custom/pixi.min.js');
+const arcade = path.join(phaserModule, 'build/custom/phaser-arcade-physics.min.js');
+
 
 module.exports = {
   context: __dirname + '/src',
@@ -14,6 +22,7 @@ module.exports = {
   },
   devServer: {
     contentBase: __dirname + "/src",
+    watchContentBase: true,
   },
   module: {
     rules: [
@@ -33,5 +42,17 @@ module.exports = {
         ]
       },
     ],
+    loaders: [
+     { test: /pixi\.js/, loader: 'expose?PIXI' },
+     { test: /phaser-split\.js$/, loader: 'expose?Phaser' },
+     { test: /p2\.js/, loader: 'expose?p2' }
+   ]
+  },
+  resolve: {
+    alias: {
+      'phaser': phaser,
+      'pixi': pixi,
+      'arcade': arcade
+    }
   },
 };
